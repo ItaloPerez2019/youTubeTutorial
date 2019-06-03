6fcab6eb1d9fd3edf92b0b437172ad930f4bbd86
italo <iep4.njit.edu>
2019-06-03 10:18:56 -0400

    'myproject'

---
+++ django_project/django_onlineshoppingstore/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/italoperez/Desktop/back_up/django_youtubeapp/django_project/django_onlineshoppingstore/venv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/italoperez/Desktop/django_youtubeapp/venv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
